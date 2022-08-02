6efd82422637af7f5fc0c47d0e04d1f44f132fd7
shakiv633 <shakibshuvo633@gmail.com>
2022-08-02 12:05:23 +0600

    basic complete

---
+++ basic.js
@@ -0,0 +1,24 @@
+// prb 1
+
+var num1=3;
+var num2=2
+var sum=(num1+num2)*num1;
+console.log(sum);
+
+// prb 2 
+
+
+ num1=9;
+ num2=5
+
+ sum=(num1*num2);
+console.log(sum);
+
+
+// prb 3
+
+num1=1;
+console.log(num1);
+
+
+
